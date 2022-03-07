46f3b6b763336f7c1a42d93f452fa2c4623adce9
miunshine <paliesk_debesi@yahoo.com>
2022-03-07 12:45:43 +0000

    naming

--- ui_lineplot.R
+++ ui_lineplot.R
@@ -20,7 +20,7 @@ nnn <- dbGetQuery(con, paste0("select run_id from run_data where (key = 'tests-o
 for (i in nnn[,1]){
   l = dbGetQuery(con, paste0("select value from run_data where key in ('backend-version') and run_id = '",i,"' "))
   if (length(l[,1]) == 0){
-    line <- data.frame(i, 'backend-version', 'NA')
+    line <- data.frame(i, 'backend-version', 'unmodified')
     colnames(line) <- list('run_id','key','value')
     dbAppendTable(con, "run_data", line)
   }
